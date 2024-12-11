7c80f1d29d93933dfa499f781d17740e7ab466f8
JasperKS <kjdlsarmiento@gmail.com>
2024-12-11 06:02:39 -0500

    implemented delete and show info

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let data = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(data);
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date();
+    response.send(
+        '<div>Phonebook has info for ' + data.length + ' people' 
+        + '<br/><br/>' + date.toString() +  '</div>'
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = data.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end();
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    data = data.filter(person => person.id !== id);
+
+    response.status(204).end();
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
