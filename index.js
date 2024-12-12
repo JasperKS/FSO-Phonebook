6b712c66982d2287b6f00c2a0be1b7770576a1a9
JasperKS <kjdlsarmiento@gmail.com>
2024-12-12 05:59:31 -0500

    implemented post and error handling

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id;
-    const person = data.find(person => person.id === id)
+    console.log(typeof(id))
+    const person = data.find(person => person.id == id)
 
     if (person) {
         response.json(person)
@@ -52,9 +53,36 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id;
-    data = data.filter(person => person.id !== id);
+    data = data.filter(person => person.id != id);
 
     response.status(204).end();
+})
+
+app.post('/api/persons', (request, response) => {
+    const new_id = Math.floor(Math.random() * 10000)
+    const body = request.body;
+
+    if (!body.name && !body.number) {
+        return response.status(400).json({
+            error: 'name or number is missing'
+        })
+    }
+
+    if (data.some(person => person.name == body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: new_id,
+        name: body.name,
+        number: body.number,   
+    }
+
+    data = data.concat(person)
+    response.json(person)
+
 })
 
 const PORT = 3001
