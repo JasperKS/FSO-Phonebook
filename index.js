3c542f497906a6f484df69d645d64bbf80696fb1
JasperKS <kjdlsarmiento@gmail.com>
2025-02-19 05:51:53 -0500

    all routes now work with the backend

--- index.js
+++ index.js
@@ -13,29 +13,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 let data = []
 
-// let data = [
-//     { 
-//       "id": "1",
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": "2",
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": "3",
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": "4",
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ]
-
 app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
@@ -66,29 +43,22 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     const date = new Date();
-    response.send(
-        '<div>Phonebook has info for ' + data.length + ' people' 
-        + '<br/><br/>' + date.toString() +  '</div>'
-    )
+
+    Person.countDocuments({})
+        .then(count => {
+            response.send(
+                '<div>Phonebook has info for ' + count + ' people' 
+                + '<br/><br/>' + date.toString() +  '</div>')
+        })
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id;
-    console.log(typeof(id))
-    const person = data.find(person => person.id == id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end();
-    }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    data = data.filter(person => person.id != id);
-
-    response.status(204).end();
+    Person.findById(id)
+        .then(foundPerson => {
+            response.json(foundPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -118,6 +88,42 @@ app.post('/api/persons', (request, response) => {
         response.json(person)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new : true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    Person.findByIdAndDelete(id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
--- models/phonebook.js
+++ models/phonebook.js
@@ -28,33 +28,3 @@ personSchema.set('toJSON', {
 const Person = mongoose.model('Person', personSchema)
 
 module.exports = mongoose.model('Person', personSchema)
-
-// if (process.argv.length == 5) {
-//     let name = process.argv[3]
-//     let number = process.argv[4]
-
-//     const person = new Person({
-//         name: name,
-//         number: number,
-//     })
-
-//     person.save().then(result => {
-//         console.log(`added ${result.name} number ${result.number} to phonebook`)
-//         mongoose.connection.close()
-//     })
-    
-// } else if (process.argv.length == 3) {
-//     Person.find({}).then(result => {
-//         console.log("phonebook:")
-//         result.forEach(person => {
-//             console.log(`${person.name} ${person.number}`)
-//         })
-//         mongoose.connection.close()
-//     })
-    
-// } else {
-//     console.log("wrong number of arguments")
-//     mongoose.connection.close()
-// }
-
-
